57a1baa54f508ae4ae104095dcf0f735c67d5293
RidwanSiddique44 <srsiddique118@gmail.com>
2022-03-01 11:39:38 +0600

    details loaded

--- JS/mobile.js
+++ JS/mobile.js
@@ -12,7 +12,7 @@ const loadData = () => {
 //-------------- Display Result Function -------------//
 const displayResult = (phones) => {
     const result = document.getElementById('display-result');
-    result.textContent = '';
+
     phones.forEach(phone => {
         const div = document.createElement('div');
         div.classList.add('col');
@@ -21,10 +21,31 @@ const displayResult = (phones) => {
             <div class="card-body">
                 <h5 class="card-title px-5">${phone.brand}</h5>
                 <p class="card-text px-5">${phone.phone_name}</p>
+                <button onclick="loadDetails('${phone.slug}')" type="button" class="btn btn-info px-5 mx-5 w-75">Details</button>
             </div>
         </div>`
         result.appendChild(div);
 
     });
+
+}
+const loadDetails = id => {
+    const url = ` https://openapi.programming-hero.com/api/phone/${id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayDitails(data.data))
+
+}
+const displayDitails = info => {
+    console.log(info)
+    const result = document.getElementById('display-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `<img src="${info.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>`
+    result.appendChild(div);
 
 }
